a1afaadcb1da97bd8d7c51e78e49d9f2ec78b751
WooSangWook <raincoat@kakao.com>
2024-04-09 12:02:29 +0900

    Feat: lending-test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,46 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
+    <q-header
+      elevated
+      @mouseenter="activateNav"
+      @mouseleave="deactivateNav"
+      class="bg-white text-black fixed"
+      style="height: 88px; z-index: 1"
+    >
+      <q-toolbar class="row justify-between">
+        <div class="row">
+          <div
+            class="row items-center justify-center q-mr-lg"
+            style="height: 88px; width: 130px"
+          >
+            <q-img src="../assets/logo.png" spinner-color="white" />
+          </div>
+          <ul
+            class="row q-ma-none q-pa-none"
+            style="height: 88px; list-style: none"
+          >
+            <li
+              class="full-height"
+              style="width: 120px"
+              v-for="(item, index) in menuItems"
+              :key="index"
+            >
+              <div class="full-height row items-center justify-center q-px-md">
+                <span class="text-h6">{{ item.title }}</span>
+              </div>
+              <ul v-if="navActive" class="q-pt-sm" style="color: black">
+                <li
+                  class="q-px-none q-py-sm text-center"
+                  v-for="(subItem, subIndex) in item.subMenu"
+                  :key="`subItem-${subIndex}`"
+                >
+                  <a :href="subItem.link">{{ subItem.name }}</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+
         <q-btn
           flat
           dense
@@ -10,93 +49,99 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+      <div
+        class="full-width bg-white"
+        style="height: 15rem"
+        v-if="navActive"
+      ></div>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <q-drawer v-model="rightDrawerOpen" bordered side="right"> </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <div class="text-h6 bg-black text-white full-width">Footer</div>
   </q-layout>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+const navActive = ref(false);
 
-const leftDrawerOpen = ref(false)
+const rightDrawerOpen = ref(false);
 
 function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+  rightDrawerOpen.value = !rightDrawerOpen.value;
 }
+
+const menuItems = ref([
+  {
+    title: '병원소개',
+    subMenu: [
+      { name: '리팅의원소개', link: '#' },
+      { name: '대표원장 스토리', link: '#' },
+      { name: '의료진 소개', link: '#' },
+      { name: '진료안내/오시는길', link: '#' },
+      { name: '병원둘러보기', link: '#' },
+    ],
+  },
+  {
+    title: '실리프팅',
+    subMenu: [
+      { name: '리팅 실 리프팅', link: '#' },
+      { name: '슬리핑실', link: '#' },
+    ],
+  },
+  {
+    title: '줄기세포',
+    subMenu: [
+      { name: '셀프팅', link: '#' },
+      { name: '셀스킨주사', link: '#' },
+    ],
+  },
+  {
+    title: '쁘띠리프팅',
+    subMenu: [
+      { name: '레이저 리프팅', link: '#' },
+      { name: '리팅부스터', link: '#' },
+    ],
+  },
+  {
+    title: '필러보톡스',
+    subMenu: [{ name: '필러보톡스', link: '#' }],
+  },
+  {
+    title: '지방흡입',
+    subMenu: [{ name: '초음파 지방흡입', link: '#' }],
+  },
+  {
+    title: '커뮤니티',
+    subMenu: [
+      { name: '공지사항', link: '#' },
+      { name: '리팅뉴스', link: '#' },
+      { name: '전후사진', link: '#' },
+      { name: '자필후기', link: '#' },
+    ],
+  },
+  {
+    title: '상담예약',
+    subMenu: [
+      { name: '온라인상담', link: '#' },
+      { name: '비용상담', link: '#' },
+      { name: '이벤트', link: '#' },
+    ],
+  },
+]);
+
+const activateNav = () => {
+  navActive.value = true;
+};
+
+const deactivateNav = () => {
+  navActive.value = false;
+};
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,252 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="q-pb-xl">
+    <q-carousel
+      v-model="slide"
+      transition-prev="jump-right"
+      transition-next="jump-left"
+      swipeable
+      animated
+      control-color="white"
+      prev-icon="arrow_left"
+      next-icon="arrow_right"
+      navigation-icon="radio_button_unchecked"
+      navigation
+      padding
+      arrows
+      height="90vh"
+      class="bg-purple text-white shadow-1 rounded-borders"
+    >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-icon name="style" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-icon name="terrain" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+
+    <div class="q-pa-xl column items-center justify-center">
+      <div style="width: 1200px" class="column q-gutter-xl">
+        <div class="row">
+          <div class="col q-pr-xl">
+            <h5 class="text-h5 q-pb-md">부제목 입니다</h5>
+            <h3 class="text-h3 q-pb-xl text-bold">제목입니다</h3>
+            <spa class="text-body1">
+              설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘
+              보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요?
+              설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘
+              보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요?
+              설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘
+              보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요?
+              설명인데요 잘 보이시나요?
+            </spa>
+          </div>
+          <div class="col q-pl-xl">
+            <q-img
+              class="col-5"
+              src="https://cdn.quasar.dev/img/parallax1.jpg"
+            />
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col q-pr-xl">
+            <q-img
+              class="col-5"
+              src="https://cdn.quasar.dev/img/parallax1.jpg"
+            />
+          </div>
+          <div class="col q-pl-xl">
+            <h5 class="text-h5 q-pb-md">부제목 입니다</h5>
+            <h3 class="text-h3 q-pb-xl text-bold">제목입니다</h3>
+            <spa class="text-body1">
+              설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘
+              보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요?
+              설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘
+              보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요?
+              설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘
+              보이시나요? 설명인데요 잘 보이시나요? 설명인데요 잘 보이시나요?
+              설명인데요 잘 보이시나요?
+            </spa>
+          </div>
+        </div>
+
+        <div class="row q-gutter-xl">
+          <div class="col">
+            <q-card flat bordered class="my-card">
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+
+              <q-separator inset />
+
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col">
+            <q-card flat bordered class="my-card">
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+
+              <q-separator inset />
+
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="col">
+            <q-card flat bordered class="my-card">
+              <q-card-section>
+                <div class="text-h6">Our Changing Planet</div>
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+
+              <q-separator inset />
+
+              <q-card-section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col q-pr-xl">
+            <div class="col-5" style="height: 400px">
+              <img
+                src="https://cdn.pixabay.com/photo/2024/03/19/15/49/woman-8643502_640.png"
+                style="height: 100%"
+              />
+            </div>
+          </div>
+          <div class="col q-pl-sm">
+            <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+              <q-input
+                filled
+                v-model="name"
+                label="Your name *"
+                hint="Name and surname"
+                lazy-rules
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type something',
+                ]"
+              />
+
+              <q-input
+                filled
+                type="number"
+                v-model="age"
+                label="Your age *"
+                lazy-rules
+                :rules="[
+                  (val) =>
+                    (val !== null && val !== '') || 'Please type your age',
+                  (val) => (val > 0 && val < 100) || 'Please type a real age',
+                ]"
+              />
+
+              <q-toggle
+                v-model="accept"
+                label="I accept the license and terms"
+              />
+
+              <div>
+                <q-btn label="Submit" type="submit" color="primary" />
+                <q-btn
+                  label="Reset"
+                  type="reset"
+                  color="primary"
+                  flat
+                  class="q-ml-sm"
+                />
+              </div>
+            </q-form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <q-card-actions
+      vertical
+      class="justify-around fixed bg-white q-mb-xl"
+      style="bottom: 0; right: 0; border: 1px solid"
+    >
+      <q-btn flat round color="red" icon="favorite" size="xl" />
+      <q-btn flat round color="accent" icon="bookmark" size="xl" />
+      <q-btn flat round color="primary" icon="share" size="xl" />
+    </q-card-actions>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { ref } from 'vue';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+const slide = ref('style');
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+const name = ref(null);
+const age = ref(null);
+const accept = ref(false);
+
+function onSubmit() {
+  if (accept.value !== true) {
+    $q.notify({
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'warning',
+      message: 'You need to accept the license and terms first',
+    });
+  } else {
+    $q.notify({
+      color: 'green-4',
+      textColor: 'white',
+      icon: 'cloud_done',
+      message: 'Submitted',
+    });
   }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+}
+
+function onReset() {
+  name.value = null;
+  age.value = null;
+  accept.value = false;
+}
 </script>
